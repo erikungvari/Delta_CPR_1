4ea06a73b32c95cd50d93d5c12d737fc120a3f0c
Erik UNGVÁRI <ungver@delta-studenti.cz>
2024-11-12 11:05:23 +0100

    Dividends for all accounts

--- src/main/java/org/example/accounts/DividendService.java
+++ src/main/java/org/example/accounts/DividendService.java
@@ -8,6 +8,8 @@ import java.util.Map;
 public class DividendService {
     @Inject
     MoneyTransferService moneyTransferService;
+    @Inject
+    GlobalBankAccountStorage globalBankAccountStorage;
 
     public void getDividends(InvestingAccount account){
         double amount = 0;
@@ -16,5 +18,12 @@ public class DividendService {
             amount += share.getBalance()/100*share.getDividend();
         }
         moneyTransferService.invest(account, amount);
+    }
+    public void getDividends(){
+        for(BankAccount account : globalBankAccountStorage.bankAccountStorage){
+            if(account instanceof InvestingAccount){
+                getDividends((InvestingAccount) account);
+            }
+        }
     }
 }
